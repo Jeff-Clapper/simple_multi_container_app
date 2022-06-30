66622a85165b3fe1f4d6fd4732fcad5530b6ff4c
Jeff-Clapper <jwclapper@gmail.com>
2022-06-29 21:28:47 -0400

    final app, will need to incorporate docker next

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request
+from config.mysqlconnection import connectToMySQL
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    quotes = ["to be or not to be","winning isn't everything","quote the raven, nevermore"]
-    return render_template("index.html",quotes)
+    query = 'SELECT quote FROM quotes'
+    results = connectToMySQL().query_db(query)
+    quotes = []
+    for quote in results:
+        quotes.append(quote["quote"])
 
-@app.route("/process")
+    return render_template("index.html",quotes=quotes)
+
+@app.route("/process", methods=['POST'])
 def process():
+    data = {"quote": request.form['quote']}
+    print(data['quote'])
+    query = "INSERT INTO quotes (quote) VALUES (%(quote)s);"
+    connectToMySQL().query_db(query,data)
     return redirect("/")
 
 if __name__ == "__main__":
